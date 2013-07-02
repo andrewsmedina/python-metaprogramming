9c16aff80b0b337f43fa12d5f003691259cd1c44
Andrews Medina <andrewsmedina@gmail.com>
2013-07-02 02:48:51 -0300

    added minify decorator.

--- example1/minifier.py
+++ example1/minifier.py
@@ -6,18 +6,24 @@ def html_minify(html_code):
     return html.tostring(dom)
 
 
+def minify(func):
+    def wrapper(*args, **kwargs):
+        html_code = func(*args, **kwargs)
+        return html_minify(html_code)
+    return wrapper
+
 
 def test(code):
     return html_minify(code)
 
 
-# @minify
-# def test2(code):
-#     return code
+@minify
+def test2(code):
+    return code
 
 
 pure = "<html>    <body>  <body>   </html>"
 minified = "<html><body>     </body></html>"
 
 assert minified == test(pure)
-# assert minified == test2(pure)
+assert minified == test2(pure)
