3cb73628b273fd54752588dcc329dd2ee41b5b4a
Andrews Medina <andrewsmedina@gmail.com>
2013-07-02 20:09:15 -0300

    added ensure decorator.

--- example4/ann.py
+++ example4/ann.py
@@ -1,7 +1,18 @@
+from functools import wraps
+
+
+def ensure(func):
+    @wraps
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return func
+
+
 def add(n1: 'positive', n2: 'negative'):
     return n1 + n2
 
 
+@ensure
 def sub(n1: 'positive', n2: 'negative'):
     return n1 - n2
 
