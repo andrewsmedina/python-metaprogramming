cb8b4a8458ea2be3e3168c7404266888ea72c880
Andrews Medina <andrewsmedina@gmail.com>
2013-07-02 15:42:41 -0300

    added validators.

--- example3/validators.py
+++ example3/validators.py
@@ -1,12 +1,32 @@
-class String:
-    pass
+class Typed:
+    ty = object
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self.ty):
+            raise TypeError('Expected %s' % self.ty)
 
 
-class Integer:
-    pass
+class String(Typed):
+    ty = str
+
+
+class Integer(Typed):
+    ty = int
 
 
 class Person:
     name = String()
     age = Integer()
 
+
+person = Person()
+person.name = 'andrews'
+try:
+    person.name = 1 #ops
+except TypeError:
+    pass
+
+try:
+    person.age = "ble"
+except TypeError:
+    pass
