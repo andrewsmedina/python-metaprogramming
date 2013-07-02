7688371f36ef51dab217c24a559730d771b91369
Andrews Medina <andrewsmedina@gmail.com>
2013-07-02 20:25:10 -0300

    improved type descriptor.

--- example3/validators.py
+++ example3/validators.py
@@ -1,10 +1,13 @@
 class Typed:
     ty = object
 
+    def __init__(self, name):
+        self.name = name
+
     def __set__(self, instance, value):
         if not isinstance(value, self.ty):
             raise TypeError('Expected %s' % self.ty)
-
+        instance.__dict__[self.name] = value
 
 class String(Typed):
     ty = str
@@ -15,12 +18,13 @@ class Integer(Typed):
 
 
 class Person:
-    name = String()
-    age = Integer()
+    name = String('name')
+    age = Integer('age')
 
 
 person = Person()
 person.name = 'andrews'
+print(person.name)
 try:
     person.name = 1 #ops
 except TypeError:
